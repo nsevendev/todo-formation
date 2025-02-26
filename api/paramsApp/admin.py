6ed96e3aa32db5a6e3690b5ec5cb9dcb42769c75
jha <j.haimez@vattenfall.fr>
2025-02-26 20:45:29 +0100

    delete old project python

--- api/paramsApp/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import ParamApp
-
-admin.site.register(ParamApp)
--- api/paramsApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ParamsappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'paramsApp'
