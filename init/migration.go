75f5df09e2925ab1e78105fdaedc566d943a9735
jha <j.haimez@vattenfall.fr>
2025-03-28 14:52:36 +0100

    chore : add gestion with not files migrations

--- init/migration.go
+++ init/migration.go
@@ -17,6 +17,18 @@ func initMigration() error {
 	defer db.Close()
 
 	logger.Info("Exécution des migrations...")
+
+	_, errGoose := goose.CollectMigrations("./migrations", 0, goose.MaxVersion)
+	logger.Infof("%v", errGoose)
+	if errGoose != nil && strings.Contains(errGoose.Error(), "no migration files found") {
+		logger.Infof("Error => %v", errGoose)
+		logger.Warn("Aucune migration trouvée. Rien à faire.")
+		return nil
+	}
+
+	if errGoose != nil {
+		logger.Fatalf("Erreur en collectant les migrations: %v", errGoose)
+	}
 	
 	if err := goose.Up(db, "./migrations"); err != nil {
 		logger.Fatalf("Impossible d'exécuter les migrations: %v", err)
