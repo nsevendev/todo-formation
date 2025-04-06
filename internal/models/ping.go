592a953043213118b4e94d46e9b31abc38d573ad
jha <j.haimez@vattenfall.fr>
2025-04-06 11:12:10 +0200

    reset

--- internal/models/ping.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "time"
-
-type Ping struct {
-	ID        uint      `gorm:"primaryKey" json:"id"`
-	Message   string    `gorm:"type:varchar(255)" json:"message"`
-	CreatedAt time.Time `json:"created_at"`
-}
