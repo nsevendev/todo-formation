af7fec217bbdd40f3be84204014273ad76fda4f6
BoissonMathis <math.boiss@laposte.net>
2025-04-26 21:15:40 +0200

    add TaskUpdateDto type in task model #20

--- internal/task/taskmodel.go
+++ internal/task/taskmodel.go
@@ -20,6 +20,10 @@ type TaskCreateDto struct {
 	Label string `json:"label" validate:"required"`
 }
 
+type TaskUpdateDto struct {
+	Label string `json:"label" validate:"required"`
+}
+
 type TaskDeleteManyDto struct {
 	Ids []string `json:"ids" validate:"required"`
 }
