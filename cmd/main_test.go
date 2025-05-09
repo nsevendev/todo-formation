e45e5715fed15de29ed636d938f2fb13685ce33b
jha <j.haimez@vattenfall.fr>
2025-05-09 06:44:55 +0200

    chore : change testing main.go

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -4,9 +4,9 @@ import "testing"
 
 func TestExtractStringInBacktick(t *testing.T) {
 	tests := []struct {
-		name	string
-		value   string
-		expect  string
+		name   string
+		value  string
+		expect string
 	}{
 		{"test avec des `backtick`", "`test`", "test"},
 		{"test sans backtick", "test", ""},
@@ -14,7 +14,7 @@ func TestExtractStringInBacktick(t *testing.T) {
 
 	for _, tt := range tests {
 		result := extractStringInBacktick(tt.value)
-		if result != tt.expect{
+		if result != tt.expect {
 			t.Errorf("%s: got %v, expect %v", tt.name, result, tt.expect)
 		}
 	}
