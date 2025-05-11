fa28cb7df06379fe554ab8fd3baafe890116c1d0
BoissonMathis <math.boiss@laposte.net>
2025-05-11 15:20:06 +0200

    add test function login in auth service test #29

--- internal/auth/service_test.go
+++ internal/auth/service_test.go
@@ -35,3 +35,37 @@ func TestRegister(t *testing.T){
 	}
 }
 
+func TestLogin(t *testing.T){
+	tests := []struct {
+		name string
+		email string
+		password string
+		isToken bool
+		isErr bool
+	}{
+		{"test success", "admin@gmail.com", "password", true, false},
+		{"test success", "fail@gmail.com", "password", false, true},
+		{"test success", "admin@gmail.com", "invalid password", false, true},
+	}
+
+	for _, tt := range tests {
+		userLoginDto := UserLoginDto{
+			Email: tt.email,
+			Password: tt.password,
+		}
+
+		token, err := s.Login(ctx, userLoginDto)
+
+		if (err != nil) != tt.isErr {
+			t.Errorf("%s: got error %v, expect error %v", tt.name, err, tt.isErr)
+		}
+
+		if (err == nil) == tt.isErr {
+			t.Errorf("%s: got error %v, expect error %v", tt.name, err, tt.isErr)
+		}
+
+		if (token == "") == tt.isToken{
+			t.Errorf("%s: login fail with email %v and password %v", tt.name, tt.email, tt.password)
+		}
+	}
+}
