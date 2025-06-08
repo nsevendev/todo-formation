c3cb48a89979a1cccba6070d8248111cc5732fd0
jha <j.haimez@vattenfall.fr>
2025-06-08 14:52:03 +0200

    add host dynamique for swagger

--- docs/docs.go
+++ docs/docs.go
@@ -794,7 +794,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "todof.local",
+	Host:             "",
 	BasePath:         "",
 	Schemes:          []string{"https"},
 	Title:            "API todo-formation",
